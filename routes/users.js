113b3a61f979895a97c9f179f7b7f57ac0d9dbe3
sunghyunpark <sunghyun1038@naver.com>
2018-09-15 23:30:02 +0900

    api test

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,4 @@
-module.exports = function(app)
+module.exports = function(app, User)
 {
   app.get('/', function(req, res) {
 
@@ -19,5 +19,5 @@ module.exports = function(app)
       res.json({result : 1});
     })
   });
-  
+
 }
