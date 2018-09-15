1aff68df8220cf19dbdd0958e88e081657cdc499
sunghyunpark <sunghyun1038@naver.com>
2018-09-15 23:47:17 +0900

    api test

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,23 @@
-module.exports = function(app, User)
-{
-  app.get('/', function(req, res) {
+var express = require('express');
+var bodyParser = require('body-parser');
+var router = express.Router();
 
-  });
+router.get('/', function(req, res) {
 
-  app.post('/', function(req, res) {
-    var User = new User();
-    user.uid = req.body.uid;
-    user.nickname = req.body.nickName;
+});
 
-    user.save(function(err){
-      if(err){
-        console.log(err);
-        res.json({result : 0});
-        return;
-      }
+router.post('/', function(req, res) {
+  var User = new User();
+  user.uid = req.body.uid;
+  user.nickname = req.body.nickName;
 
-      res.json({result : 1});
-    })
-  });
+  user.save(function(err){
+    if(err){
+      console.log(err);
+      res.json({result : 0});
+      return;
+    }
 
-}
+    res.json({result : 1});
+  })
+});
